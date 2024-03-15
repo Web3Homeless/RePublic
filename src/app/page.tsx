9c0679a285cb0baefed87855876842502cc36f48
Ilya Klimenko <sn0wy-0wl@yandex.ru>
2024-03-16 00:34:28 +0300

    rm showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,26 +54,7 @@ export default async function Home() {
             </Link>
           </div>
         </div>
-
-        <CrudShowcase />
       </div>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
-
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-    </div>
   );
 }

5f316f31a6be17c3f5587d942727d6a09784669b
Ilya Klimenko <sn0wy-0wl@yandex.ru>
2024-03-16 00:32:00 +0300

    rm t3stack showcase, change main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
+import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+  noStore();
   const session = await getServerAuthSession();
+  console.log(session);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-start text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
@@ -66,7 +67,7 @@ async function CrudShowcase() {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
 
-  const latestPost = await api.post.getLatest();
+  const latestPost = await api.post.getLatest.query();
 
   return (
     <div className="w-full max-w-xs">
@@ -75,8 +76,6 @@ async function CrudShowcase() {
       ) : (
         <p>You have no posts yet.</p>
       )}
-
-      <CreatePost />
     </div>
   );
 }

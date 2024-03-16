dd7b954888d0b0acbc3e18eb840397f2a9c8e1b5
Ilya Klimenko <sn0wy-0wl@yandex.ru>
2024-03-16 15:48:01 +0300

    ignore ts errors

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default config;

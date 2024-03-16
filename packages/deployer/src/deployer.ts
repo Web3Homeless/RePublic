77cec9b34c9031486a4acc3d24dcc2b9f75d2f24
asimaranov <a.simaranov@gmail.com>
2024-03-16 13:51:46 +0000

    Working deploy for rust

--- packages/deployer/src/deployer.ts
+++ packages/deployer/src/deployer.ts
@@ -4,10 +4,25 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import decompress from 'decompress';
 import * as uuid from 'uuid';
-import { spawn } from 'child_process';
+import { spawn, exec } from 'child_process';
 
 const deploy = async (argv: { projectZip: string }) => {
-  const dirName = `temp/${uuid.v4()}`;
+  const uuidTag = uuid.v4();
+
+  const dirName = `temp/${uuidTag}`;
+  const params = { cwd: `${dirName}/contract-rs` };
+  const account = `${uuidTag}.testnet`
+
+  console.log('Creating account..');
+
+  await new Promise(function (resolve, reject) {
+    const createAccount = exec(
+      `near create-account ${account} --useFaucet`
+    );
+
+    createAccount.addListener('error', reject);
+    createAccount.addListener('exit', resolve);
+  });
 
   console.log('Deploying', argv.projectZip, dirName);
 
@@ -15,7 +30,7 @@ const deploy = async (argv: { projectZip: string }) => {
 
   console.log('Execution', `${dirName}/contract-rs/test.sh`);
 
-  const buildTask = spawn(`./build.sh`, [], { cwd: `${dirName}/contract-rs` });
+  const buildTask = spawn(`./build.sh`, [], params);
 
   buildTask.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
@@ -28,10 +43,23 @@ const deploy = async (argv: { projectZip: string }) => {
   buildTask.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
 
-    const deployTask = spawn(`near contract deploy republic.testnet use-file ./target/wasm32-unknown-unknown/release/*.wasm without-init-call network-config testnet sign-with-keychain send`, [], { cwd: `${dirName}/contract-rs` });
-  });
+    const deployTask = spawn(
+      `sh`,
+      [
+        '-c',
+        `near deploy ${account} ./target/wasm32-unknown-unknown/release/*.wasm`,
+      ],
+      params
+    );
 
-  // await test;
+    deployTask.stdout.on('data', (data) => {
+      console.log(`Deploy stdout: ${data}`);
+    });
+
+    deployTask.stderr.on('data', (data) => {
+      console.log(`Deploy stderr: ${data}`);
+    });
+  });
 };
 
 // eslint-disable-next-line max-len
@@ -39,6 +67,16 @@ const deploy = async (argv: { projectZip: string }) => {
 yargs(hideBin(process.argv))
   .command(
     'deploy [projectZip]',
+    'Deploys specified project',
+    {
+      projectZip: {
+        default: 'data/contract-rs.zip',
+      },
+    },
+    deploy
+  )
+  .command(
+    'deploySolFile [projectZip]',
     'Deploys specified project',
     {
       projectZip: {
---
+++ packages/deployer/src/lib/crosschain_deployer.ts
@@ -0,0 +1,129 @@
+import { Web3 } from 'web3';
+import { bytesToHex } from '@ethereumjs/util';
+import { FeeMarketEIP1559Transaction } from '@ethereumjs/tx';
+import { Common } from '@ethereumjs/common';
+import fs from 'fs';
+
+export class Ethereum {
+  web3: Web3;
+  chain_id: any;
+  constructor(chain_rpc: any, chain_id: any) {
+    this.web3 = new Web3(chain_rpc);
+    this.chain_id = chain_id;
+  }
+
+  async queryGasPrice() {
+    const res = await fetch(
+      'https://sepolia.beaconcha.in/api/v1/execution/gasnow'
+    );
+    const json = await res.json();
+    return json.data.rapid;
+  }
+
+  async getBalance(accountId: any) {
+    const balance = await this.web3.eth.getBalance(accountId);
+    const ONE_ETH = 1000000000000000000n;
+    return Number((balance * 100n) / ONE_ETH) / 100;
+  }
+
+  async deployContract(sender: string, tokenAbi: string, bytecode: string, args: any[]) {
+    const common = new Common({ chain: this.chain_id });
+    let abi = JSON.parse(fs.readFileSync(tokenAbi).toString());
+    let contractInstance = new this.web3.eth.Contract(abi);
+
+    let deploy = contractInstance
+      .deploy({
+        data: bytecode,
+        arguments: args,
+      })
+      .encodeABI();
+
+    // Get the nonce
+    const nonce = await this.web3.eth.getTransactionCount(sender);
+    const maxFeePerGas = await this.queryGasPrice();
+
+    // Construct transaction
+    const transactionData = {
+      nonce,
+      gasLimit: 21000,
+      maxFeePerGas,
+      maxPriorityFeePerGas: 1,
+      value: BigInt(0),
+      chain: this.chain_id,
+      datda: ''
+    };
+
+    // Return the message hash
+    const transaction = FeeMarketEIP1559Transaction.fromTxData(
+      transactionData,
+      { common }
+    );
+    const payload = Array.from(
+      new Uint8Array(transaction.getHashedMessageToSign().slice().reverse())
+    );
+    return { transaction, payload };
+  }
+
+  async createPayload(sender: any, receiver: any, amount: any) {
+    const common = new Common({ chain: this.chain_id });
+
+    // Get the nonce
+    const nonce = await this.web3.eth.getTransactionCount(sender);
+    const maxFeePerGas = await this.queryGasPrice();
+
+    // Construct transaction
+    const transactionData = {
+      nonce,
+      gasLimit: 21000,
+      maxFeePerGas,
+      maxPriorityFeePerGas: 1,
+      to: receiver,
+      value: BigInt(this.web3.utils.toWei(amount, 'ether')),
+      chain: this.chain_id,
+    };
+
+    // Return the message hash
+    const transaction = FeeMarketEIP1559Transaction.fromTxData(
+      transactionData,
+      { common }
+    );
+    const payload = Array.from(
+      new Uint8Array(transaction.getHashedMessageToSign().slice().reverse())
+    );
+    return { transaction, payload };
+  }
+
+  reconstructSignature(
+    transaction: {
+      addSignature: (arg0: bigint, arg1: Buffer, arg2: Buffer) => any;
+    },
+    big_r: string,
+    big_s:
+      | WithImplicitCoercion<string>
+      | { [Symbol.toPrimitive](hint: 'string'): string },
+    eth_sender: string
+  ) {
+    const r = Buffer.from(big_r.substring(2), 'hex');
+    const s = Buffer.from(big_s, 'hex');
+
+    const candidates = [0n, 1n].map((v) => transaction.addSignature(v, r, s));
+    const signature = candidates.find(
+      (c) =>
+        c.getSenderAddress().toString().toLowerCase() ===
+        eth_sender.toLowerCase()
+    );
+
+    if (!signature) {
+      throw new Error('Signature is not valid');
+    }
+
+    return signature;
+  }
+
+  // This code can be used to actually relay the transaction to the Ethereum network
+  async relayTransaction(signedTransaction: { serialize: () => any }) {
+    const serializedTx = bytesToHex(signedTransaction.serialize());
+    const relayed = await this.web3.eth.sendSignedTransaction(serializedTx);
+    return relayed.transactionHash;
+  }
+}
